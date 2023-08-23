0fbffdfbaca22e96cf31600c00ebbc2503371eed
E.F.Rivera <eriver10@live.spcollege.edu>
2023-08-23 13:33:10 -0400

    Still 10, on to arrays

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -13,30 +13,48 @@ import UIKit
 
 class ChecklistViewController: UITableViewController {
     
-    //These variable a within this class but out of the scope of any functions/methods
-    //I would consider these global and constant (keyword let), lets see.
-    
+
+    /*
     let row0text = "Walk the dog"
     let row1text = "Brush teeth"
     let row2text = "Learn iOS development"
     let row3text = "Soccer practice"
     let row4text = "Eat ice cream"
     
-    //The book calls these instance variables.
-    //I don't recall the author introducing instance variables.
-    
-    //The var here is asking to infer the type of these variables by what is set in them.
-    //In this case false id's them as booleans (true or false).
+
     var row0checked = false
     var row1checked = false
     var row2checked = false
     var row3checked = false
     var row4checked = false
+    */
+    
+    //replaced the above with this short stack of new instance variables that include initializer methods.
+    
+    /*
+    Initializer methods create a new object instance. In the following, we create empty instances of ChecklistItem that include the default values an empty string (””) and false for checked. Previously, created in the ChecklistItem class.
+    */
+
+      var row0item = ChecklistItem()
+      var row1item = ChecklistItem()
+      var row2item = ChecklistItem()
+      var row3item = ChecklistItem()
+      var row4item = ChecklistItem()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Setting the text manually for now.
+        row0item.text = "Walk the dog"
+        row1item.text = "Brush my teeth"
+        row1item.checked = true
+        row2item.text = "Learn iOS development"
+        row2item.checked = true
+        row3item.text = "Soccer practice"
+        row4item.text = "Eat ice cream"
+        row4item.checked = true
+        
     }
     
   
@@ -57,22 +75,22 @@ class ChecklistViewController: UITableViewController {
     
     // MARK: - Table View Data Source
         
-        //To describe this part of the lesson, I would translate this as hard coded versus dynamic or constants versus variable. If I understood how to write in this language this might be the way I go about before learning the more dynamic way.
-        
+
         let label = cell.viewWithTag(1000) as! UILabel
-          if indexPath.row == 0 {
-            label.text = row0text
-          } else if indexPath.row == 1 {
-            label.text = row1text
-          } else if indexPath.row == 2 {
-            label.text = row2text
-          } else if indexPath.row == 3 {
-            label.text = row3text
-          } else if indexPath.row == 4 {
-            label.text = row4text
-          }
         
-        //calling this method come into play later when fixing the check mark problem
+        if indexPath.row == 0 {
+          label.text = row0item.text
+        } else if indexPath.row == 1 {
+          label.text = row1item.text
+        } else if indexPath.row == 2 {
+          label.text = row2item.text
+        } else if indexPath.row == 3 {
+          label.text = row3item.text
+        } else if indexPath.row == 4 {
+          label.text = row4item.text
+        }
+        
+
         configureCheckmark(for: cell, at: indexPath)
        
         
@@ -83,48 +101,19 @@ class ChecklistViewController: UITableViewController {
       _ tableView: UITableView,
       didSelectRowAt indexPath: IndexPath
     ){
-        /*
-         if let cell = tableView.cellForRow(at: indexPath) {
-         var isChecked = false
-         if indexPath.row == 0 {
-         row0checked.toggle()
-         isChecked = row0checked
-         } else if indexPath.row == 1 {
-         row1checked.toggle()
-         isChecked = row1checked
-         } else if indexPath.row == 2 {
-         row2checked.toggle()
-         isChecked = row2checked
-         } else if indexPath.row == 3 {
-         row3checked.toggle()
-         isChecked = row3checked
-         } else if indexPath.row == 4 {
-         row4checked.toggle()
-         isChecked = row4checked
-         }
-         if isChecked {
-         cell.accessoryType = .checkmark
-         } else {
-         cell.accessoryType = .none
-         }
-         }
-         tableView.deselectRow(at: indexPath, animated: true)
-         }
-         */
-        
-        //Replaced above with this. Now instead of setting the checkmark, it now toggles the checked state.
+
         
         if let cell = tableView.cellForRow(at: indexPath) {
             if indexPath.row == 0 {
-                row0checked.toggle()
+              row0item.checked.toggle()
             } else if indexPath.row == 1 {
-                row1checked.toggle()
+              row1item.checked.toggle()
             } else if indexPath.row == 2 {
-                row2checked.toggle()
+              row2item.checked.toggle()
             } else if indexPath.row == 3 {
-                row3checked.toggle()
+              row3item.checked.toggle()
             } else if indexPath.row == 4 {
-                row4checked.toggle()
+              row4item.checked.toggle()
             }
             configureCheckmark(for: cell, at: indexPath)
         }
@@ -132,30 +121,25 @@ class ChecklistViewController: UITableViewController {
         
     }
         
-    /*
-     This function/method fixes our check mark problem where the the initial setting (bool) of the check mark was set when it is called from:
-     override func tableView(_ tableView:
-     
-     I encountered a similar issue in vscode where the solution was to toggle a boolean value under the objects attribute, what we call attribute inspector. 
 
-     */
     
     func configureCheckmark(
       for cell: UITableViewCell,
       at indexPath: IndexPath
     ){
     var isChecked = false
-      if indexPath.row == 0 {
-          isChecked = row0checked
-         } else if indexPath.row == 1 {
-           isChecked = row1checked
-         } else if indexPath.row == 2 {
-           isChecked = row2checked
-         } else if indexPath.row == 3 {
-           isChecked = row3checked
-         } else if indexPath.row == 4 {
-           isChecked = row4checked
-         }
+        
+        if indexPath.row == 0 {
+          isChecked = row0item.checked
+        } else if indexPath.row == 1 {
+          isChecked = row1item.checked
+        } else if indexPath.row == 2 {
+          isChecked = row2item.checked
+        } else if indexPath.row == 3 {
+          isChecked = row3item.checked
+        } else if indexPath.row == 4 {
+          isChecked = row4item.checked
+        }
          if isChecked {
            cell.accessoryType = .checkmark
        } else {
@@ -164,30 +148,20 @@ class ChecklistViewController: UITableViewController {
        }
 }
 
+
 /*
- At this point the book goes into how Swift, or rather objective c, organizes its function/methods' syntax.
+ Next, we tidied up all the errored if/else logic. Now, instead of separate row0text and row0checked variables,  row0item.text and row0item.checked are in play.
  
- Instead of the way I might be used seeing a function called:
+ If you've used empty "" quotes to zero out a label or text field in the past, there is no mystery on why the labels have disappeared when running the app.
  
-    configureCheckmark(someCell, someIndexPath)
+ Remember:
  
- It formats like so:
+ class ChecklistItem {
+   var text = ""
+   var checked = false //initial toggle is false or hidden.
+ }
  
-    configureCheckmark(for: someCell, at: someIndexPath)
  
- The prepositions for, at, and with are required in the Arguments section. They add bit of english and more understanding of what is being asked (or called) of the function.
  
- On the other end, at the actual function, the parameters look this.
- 
- func configureCheckmark( for cell: UITableViewCell,
-    at indexPath: IndexPath ){
-    
-    //This is inside. Note the internal name of the variable
-    if indexPath.row == 0 {...
- 
-    ||side note ==
- 
-    For me, my best translation would be: this function excepts cell of type UITableViewCell and indexPath of type IndexPath
-    However, I'm still getting used it may change it in the future or I'm wrong altogether.
  
  */
---
+++ Checklists/Checklistitem.swift
@@ -0,0 +1,25 @@
+//
+//  Checklistitem.swift
+//  Checklists
+//
+//  Created by Grey on 8/23/23.
+//
+
+import Foundation
+
+//Theres a milestone, my first class in Swift!
+
+class ChecklistItem {
+  var text = ""
+  var checked = false
+}
+
+/*
+ It's important to note that the book states that the terms property and instance variable are interchangeable (in Swift but not objective-c). And, that both to refer to an object’s data items.
+ Quoting directly it says:
+    
+ "In Swift terminology, a property is a variable or constant that is used in the context of an object. That’s exactly what an instance variable is."
+ 
+ This answers my earlier question on instance variables.
+ 
+ */
