d566621457043641c59f8b87977fadaf3febed4e
E.F.Rivera <eriver10@live.spcollege.edu>
2023-08-23 12:39:25 -0400

    Still on 10, this is Second iteration

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -5,44 +5,47 @@
 //  Created by Grey on 8/22/23.
 //
 
-/*
- Imports add extra functionality to the code. Interestingly, these may be added to the language properly in the future or kept imports depending on the use case.
- */
+
 
 import UIKit
 
 
 
 class ChecklistViewController: UITableViewController {
+    
+    //These variable a within this class but out of the scope of any functions/methods
+    //I would consider these global and constant (keyword let), lets see.
+    
+    let row0text = "Walk the dog"
+    let row1text = "Brush teeth"
+    let row2text = "Learn iOS development"
+    let row3text = "Soccer practice"
+    let row4text = "Eat ice cream"
+    
+    //The book calls these instance variables.
+    //I don't recall the author introducing instance variables.
+    
+    //The var here is asking to infer the type of these variables by what is set in them.
+    //In this case false id's them as booleans (true or false).
+    var row0checked = false
+    var row1checked = false
+    var row2checked = false
+    var row3checked = false
+    var row4checked = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    /*
-     So far our class has not discussed Overrides. It is my understanding that this function is inherited from its class, that is a rule set by the class it requires it to exist as perhaps a default. Then is up to the programmer to override or to specify what the function does in their new object.
-    */
-    
-    /*
-     What's notable about these functions is that they carry more than one parameter or inputs if you will. And they are functions that return a value, or output.
-     */
-     
-    /*
-     I am  unfamiliar with this syntax using the colon. I am fairly new to programming. It may suggest something along the lines of get this from this and be this variable. Then the inside mechanics of the function manipulate those values and send out, return that is, a value. In this case it most likely returns a single row?
-     */
+  
      
     override func tableView(
       _ tableView: UITableView,
       numberOfRowsInSection section: Int
     ) -> Int {
-    return 100 }//Amended to display this many rows
-    
-    // MARK: - Table View Data Source
-    
-    /*
-     The above comment is using a keyword "mark." It is used an indicator to find this passage.
-     This is reminiscent of "TODO and FIXME" highlighting found in VSCode.
-     */
+    return 5 }//Amended to display this many rows
     
     override func tableView(
       _ tableView: UITableView,
@@ -51,68 +54,140 @@ class ChecklistViewController: UITableViewController {
       let cell = tableView.dequeueReusableCell(
         withIdentifier: "ChecklistItem",
         for: indexPath)
+    
+    // MARK: - Table View Data Source
         
-        /*
-         Previously, we set the View,Tag with the unique identifier 1000. Then we added the following logic to this code to set the cell labels:
-        
-         
-         To quote the book " Using tags is a handy trick to get a reference to a UI element without having to make an @IBOutlet variable for it."
-         */
-        
+        //To describe this part of the lesson, I would translate this as hard coded versus dynamic or constants versus variable. If I understood how to write in this language this might be the way I go about before learning the more dynamic way.
         
         let label = cell.viewWithTag(1000) as! UILabel
+          if indexPath.row == 0 {
+            label.text = row0text
+          } else if indexPath.row == 1 {
+            label.text = row1text
+          } else if indexPath.row == 2 {
+            label.text = row2text
+          } else if indexPath.row == 3 {
+            label.text = row3text
+          } else if indexPath.row == 4 {
+            label.text = row4text
+          }
         
-        
-        if indexPath.row % 5 == 0 {
-          label.text = "Walk the dog"
-        } else if indexPath.row % 5 == 1 {
-          label.text = "Brush my teeth"
-        } else if indexPath.row % 5 == 2 {
-          label.text = "Learn iOS development"
-        } else if indexPath.row % 5 == 3 {
-          label.text = "Soccer practice"
-        } else if indexPath.row % 5 == 4 {
-          label.text = "Eat ice cream"
-        }
-        
-        //Note: as always we start with the number zero.
-        //The Book changed the code above to introduce the modulo operator. Is is Typo? I've always heard it referred to as the Modulus operator.
-        
-        //Most, likely this will change to allow the user to set the labels?
+        //calling this method come into play later when fixing the check mark problem
+        configureCheckmark(for: cell, at: indexPath)
+       
         
         return cell
     }
 
-    //Reminder: the book introduces break points. 234
+    override func tableView(
+      _ tableView: UITableView,
+      didSelectRowAt indexPath: IndexPath
+    ){
+        /*
+         if let cell = tableView.cellForRow(at: indexPath) {
+         var isChecked = false
+         if indexPath.row == 0 {
+         row0checked.toggle()
+         isChecked = row0checked
+         } else if indexPath.row == 1 {
+         row1checked.toggle()
+         isChecked = row1checked
+         } else if indexPath.row == 2 {
+         row2checked.toggle()
+         isChecked = row2checked
+         } else if indexPath.row == 3 {
+         row3checked.toggle()
+         isChecked = row3checked
+         } else if indexPath.row == 4 {
+         row4checked.toggle()
+         isChecked = row4checked
+         }
+         if isChecked {
+         cell.accessoryType = .checkmark
+         } else {
+         cell.accessoryType = .none
+         }
+         }
+         tableView.deselectRow(at: indexPath, animated: true)
+         }
+         */
+        
+        //Replaced above with this. Now instead of setting the checkmark, it now toggles the checked state.
+        
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if indexPath.row == 0 {
+                row0checked.toggle()
+            } else if indexPath.row == 1 {
+                row1checked.toggle()
+            } else if indexPath.row == 2 {
+                row2checked.toggle()
+            } else if indexPath.row == 3 {
+                row3checked.toggle()
+            } else if indexPath.row == 4 {
+                row4checked.toggle()
+            }
+            configureCheckmark(for: cell, at: indexPath)
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+        
+    /*
+     This function/method fixes our check mark problem where the the initial setting (bool) of the check mark was set when it is called from:
+     override func tableView(_ tableView:
+     
+     I encountered a similar issue in vscode where the solution was to toggle a boolean value under the objects attribute, what we call attribute inspector. 
+
+     */
     
-    /*Note to self:
-    
-     func tableView(_ tableView: UITableView,
-       numberOfRowsInSection section: Int
-    
-     is comparable to the following in other languages:
-     
-    Int numberOfRowsInSection(UITableView tableView, Int section) {
-     
-     
-    Quote from book:
-     
-     "The _ underscore is used when you don’t want a parameter to have an external name.
-     
-     ...the _ on the first parameter of methods that come from Objective-C frameworks. With such methods the first parameter only has one name but the other parameters have two.
-     
-     Sometimes people with experience in other languages get confused because they think that ChecklistViewController.swift contains three functions that are all named tableView().
-     
-     ...in Swift: the names of the parameters are part of the full method name."
-     
-     For example,
-     
-     "tableView(_:numberOfRowsInSection:)
-     tableView(_:cellForRowAt:)
-     tableView(_:didSelectRowAt:),"
-     
-     these are three different names.
-     
-    */
+    func configureCheckmark(
+      for cell: UITableViewCell,
+      at indexPath: IndexPath
+    ){
+    var isChecked = false
+      if indexPath.row == 0 {
+          isChecked = row0checked
+         } else if indexPath.row == 1 {
+           isChecked = row1checked
+         } else if indexPath.row == 2 {
+           isChecked = row2checked
+         } else if indexPath.row == 3 {
+           isChecked = row3checked
+         } else if indexPath.row == 4 {
+           isChecked = row4checked
+         }
+         if isChecked {
+           cell.accessoryType = .checkmark
+       } else {
+           cell.accessoryType = .none
+         }
+       }
 }
 
+/*
+ At this point the book goes into how Swift, or rather objective c, organizes its function/methods' syntax.
+ 
+ Instead of the way I might be used seeing a function called:
+ 
+    configureCheckmark(someCell, someIndexPath)
+ 
+ It formats like so:
+ 
+    configureCheckmark(for: someCell, at: someIndexPath)
+ 
+ The prepositions for, at, and with are required in the Arguments section. They add bit of english and more understanding of what is being asked (or called) of the function.
+ 
+ On the other end, at the actual function, the parameters look this.
+ 
+ func configureCheckmark( for cell: UITableViewCell,
+    at indexPath: IndexPath ){
+    
+    //This is inside. Note the internal name of the variable
+    if indexPath.row == 0 {...
+ 
+    ||side note ==
+ 
+    For me, my best translation would be: this function excepts cell of type UITableViewCell and indexPath of type IndexPath
+    However, I'm still getting used it may change it in the future or I'm wrong altogether.
+ 
+ */
